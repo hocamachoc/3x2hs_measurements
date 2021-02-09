10b244df5ac809e130a532110a4ac171aa2e26d2
Hugo Camacho <hocamachoc@gmail.com>
2021-02-09 13:52:09 -0800

    Change `conda activate` for `source activate`

--- etc/btrap_csis.sh
+++ etc/btrap_csis.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# 0) Load the conda (on grid we use miniconda/3)
+# 0) Load the conda module
+# Currently supporting
+# - GRID/UNESP: miniconda/3
+# - NERSC/CORI: python (anaconda3)
 case ${1} in
     grid)
 	module load miniconda/3
@@ -13,7 +16,6 @@ case ${1} in
 	exit 1
 	;;
 esac
-conda remove --name 3x2pths --all -y
 
 # 1) Clone CosmoSIS repos
 # You will need to pass de DES password for the cosmosis-des-library
@@ -25,56 +27,24 @@ git clone http://bitbucket.org/joezuntz/cosmosis-standard-library
 git clone https://darkenergysurvey@bitbucket.org/joezuntz/cosmosis-des-library
 cd ../../../
 
-# # 2) Setup conda env
-case ${1} in
-    grid)
-	# >>> conda initialize >>>
-	# !! Contents within this block are managed by 'conda init' !!
-	__conda_setup="$('/opt/gridunesp/dist/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-	if [ $? -eq 0 ]; then
-	    eval "$__conda_setup"
-	else
-	    if [ -f "/opt/gridunesp/dist/miniconda3/etc/profile.d/conda.sh" ]; then
-		. "/opt/gridunesp/dist/miniconda3/etc/profile.d/conda.sh"
-	    else
-		export PATH="/opt/gridunesp/dist/miniconda3/bin:$PATH"
-	    fi
-	fi
-	unset __conda_setup
-	# <<< conda initialize <<<
-	;;
-    cori)
-	# >>> conda initialize >>>
-	# !! Contents within this block are managed by 'conda init' !!
-	__conda_setup="$('/usr/common/software/python/3.8-anaconda-2020.11/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-	if [ $? -eq 0 ]; then
-	    eval "$__conda_setup"
-	else
-	    if [ -f "/usr/common/software/python/3.8-anaconda-2020.11/etc/profile.d/conda.sh" ]; then
-		. "/usr/common/software/python/3.8-anaconda-2020.11/etc/profile.d/conda.sh"
-	    else
-		export PATH="/usr/common/software/python/3.8-anaconda-2020.11/bin:$PATH"
-	    fi
-	fi
-	unset __conda_setup
-	# <<< conda initialize <<<
-	;;
-    *)
-	echo "Usage: ${0} <grid||cori>"
-	exit 1
-	;;
-esac
-conda deactivate
-
+# 2) Setup the conda env
+# Note the strategy here is: if there's a `3x2pths` module already is to start
+# fresh, remove it and create a new.
 sed -i -e 's/^CosmoloPy/# CosmoloPy/' etc/src/cosmosis/config/requirements.txt
 sed -i -e 's/^sklearn/scikit-learn/' etc/src/cosmosis/config/requirements.txt
-conda env create -n 3x2pths -f etc/env_conda3.yml
-conda activate 3x2pths
+conda remove --name 3x2pths --all -y
+conda env create --name 3x2pths --file etc/env_conda.yml
+source activate 3x2pths
 conda install -f etc/src/cosmosis/config/requirements.txt
 
 # 3) Install cosmosis
+# We're skipping from now `polychord` as its having some compilation issues,
+# sorry!
 sed -i -e 's/^SUBDIRS/# SUBDIRS/' etc/src/cosmosis/cosmosis/samplers/polychord/Makefile
 source etc/setup_cosmosis
 cd etc/src/cosmosis
 make
+
+# 4) Clean up
+# Just deactivate the conda env
 conda deactivate
