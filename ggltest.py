c4e7f174637ce5c9ae9a20a586c8a8754427a84d
Lucas James Faga <lucas.faga@usp.br>
2021-03-01 18:11:27 -0800

    Nersc adjustments

--- ggltest.py
+++ ggltest.py
@@ -9,7 +9,11 @@ import pymaster as nmt
 import flask
 import mcalcat
 import csh
+import healpy as hp
 
+print('BEGAN')
+
+print('config file')
 
 # Configuration file
 conf = sys.argv[1]
@@ -30,40 +34,44 @@ if conf['type'] == 'flask':
               for fn in cshcat_fn]
     ofn = f'{odir}/cls_csh_s{iseed}_ck{ick}.npz'
 elif conf['type'] == 'y1metacal':
+    print('mcal process')
     cshcat = mcalcat.mcalcat_process(conf['mcalcat'], conf['zbin'],
                                      conf['nside'])
-    ofn = f'{odir}/cls_csh_mcal.npz'
+    ofn = f'{odir}/cls_ggl_mcal.npz'
 else:
     raise ValueError(f"Computation type {conf['type']} not implemented")
+
+print('bins and effective ells')
 
 bins = nmt.NmtBin.from_edges(*np.loadtxt(conf['elledges'], unpack=True,
                                          dtype='i4'))
 
 cls = {'ell_eff': bins.get_effective_ells()}
 
-for i in range(conf['nz_lens']):
-	wc = hp.read_map(f"{conf['redmagic']}/wcountsmap_zbin{i}.fits")
-	nc = hp.read_map(f"{conf['redmagic']}/ncountsmap_zbin{i}.fits")
-	w = np.zeros_like(nc)
-	nbar = np.zeros(len(nc))
-	for z in range(0,5):
-    		for i in range(len(w[z])):
-        		if wc[z][i] or nc[z][i] != 0:
-            			w[z][i] = wc[z][i]/nc[z][i]
-		nbar[z] = sum(wc[z])/sum(w[z])
-	print(f'nbar from redmagic is {nbar}')
-	del(nc)
+print('starting zbin loops')
 
-	dmap = np.zeros_like(w)
-	for z in range(0,5):
-    		dmap[z][w[z]>0] = wc[z][w[z]>0]/(nbar[z]*w[z][w[z]>0]) - 1
-	del(wc)
-	del(w)
+for i in range(conf['nz_lens']):
+    wc = hp.read_map(f"{conf['redmagic']}/wcountsmap_zbin{i}.fits")
+    nc = hp.read_map(f"{conf['redmagic']}/countsmap_zbin{i}.fits")
+    w = np.zeros_like(nc)
+    nbar = np.zeros(len(nc))
+    for aux in range(len(w)):
+        if wc[aux] or nc[aux] != 0:
+            w[aux] = wc[aux]/nc[aux]
+    nbar = sum(wc)/sum(w)
+    print(f'nbar from redmagic is {nbar}')
+    del(nc)
+
+    dmap = np.zeros_like(w)
+    dmap[w>0] = wc[w>0]/(nbar*w[w>0]) - 1
+    del(wc)
+    del(w)
 	
-	dmask = hp.read_map(f"{conf['redmagic']}/maskmap.fits")
+    dmask = hp.read_map(f"{conf['redmagic']}/maskmap.fits")
 	
-	field_i = nmt.NmtField(dmask, [dmap[zi]],  purify_e=False, purify_b=False)
-    for j in range(i, conf['nz_source']):
+    field_i = nmt.NmtField(dmask, [dmap],  purify_e=False, purify_b=False)
+    for j in range(conf['nz_source']):
+        print(i,j)
         cshcat_j = cshcat[j]
         cshmask_j = csh.mask_make(cshcat_j, conf['nside'])
         field_j = csh.field_make(cshcat_j, cshmask_j)
---
+++ script/submit_ggl-LJF.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL
+
+# Load conda env
+# module load miniconda/3
+module load python
+source activate 3x2pths
+
+export OMP_PROC_BIND=true
+export OMP_PLACES=threads
+export OMP_NUM_THREADS=$(lscpu| grep -e '^CPU(s):'| awk '{print $2}')
+echo "Max No. of threads: ${OMP_NUM_THREADS}"
+uname -a
+
+# FLASK
+# time python cshtest.py etc/y3flask_csh.yml ${SLURM_ARRAY_TASK_ID}
+# metacal
+time python ggltest.py etc/y1mcal_ggl-LJF.yml
+
+conda deactivate
