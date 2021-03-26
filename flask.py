a6815fcf68e62e8147ca68caa80a57b1c168837a
Hugo Camacho <hocamachoc@gmail.com>
2021-03-26 04:55:47 -0700

    Update FLASK post-processing for Y1

--- flask.py
+++ flask.py
@@ -9,9 +9,9 @@ import random as rd
 import healpix_util as hu
 import itertools as it
 import multiprocessing as mp
+import yaml
 import os
 from argparse import ArgumentParser
-import sys
 
 
 def cshcat_make(map_fn, nbar, sigma_e, fgoodmap_fn, fgood_thold=0.0,
@@ -77,15 +77,10 @@ def ggmap_sample_positions(ip_good, nside, nbar, fgoodmap, seed=None,
     nbar_pix = nbar * hp.nside2pixarea(nside, degrees=True) * 3600.0
     nc_map = np.random.poisson(nbar_pix, len(ip_good))
 
-    ip_good_nest = hp.ring2nest(nside, ip_good)
-    ipix_nest = [rd.sample(range(ip_good_nest[i] * 4**nside_up,
-                                 (ip_good_nest[i] + 1) * 4**nside_up),
-                           nc_map[i])
-                 for i in range(len(ip_good))]
-    ipix_nest = [ip for sub in ipix_nest for ip in sub]
-
-    hpix = hu.HealPix('nest', nside * 2**nside_up)
-    return *hpix.pix2eq(ipix_nest), nc_map
+    ra, dec = ncmap_sample_positions(nc_map, ip_good, nside, fgoodmap,
+                                     nside_up=nside_up)
+    
+    return ra, dec, nc_map
 
 
 def cshcat_samplegamma(sigma_e, ngal, seed=None):
@@ -99,12 +94,13 @@ def cshcat_samplegamma(sigma_e, ngal, seed=None):
     return dgamma1, dgamma2
 
 
-def process_one_kggmap(iseed, ick, iz, flaskdir, outdir, nbar, sigma_e):
+def process_one_kggmap(iseed, ick, iz, flaskdir, outdir, nbar, sigma_e,
+                       kggpref="kappa-gamma-f10"):
     """
     Processes a single FLASK seed
     """
-    fgoodmap_fn = f"{flaskdir}/cookies/ck{ick+1}_desy3_goldv2p2p1.fits.gz"
-    map_fn = f"{flaskdir}/4096/seed{iseed+1}/kappa-gamma-f10z{iz+1}.fits"
+    fgoodmap_fn = f"{flaskdir}/cookies/ck{ick+1}.fits.gz"
+    map_fn = f"{flaskdir}/4096/seed{iseed+1}/{kggpref}z{iz+1}.fits"
     cat_fn = f"{outdir}/srccat_z{iz+1}_s{iseed+1}_ck{ick+1}.parquet"
 
     cshcat = cshcat_make(map_fn, nbar, sigma_e, fgoodmap_fn)
@@ -160,6 +156,72 @@ def process_lenscat(iseed, flaskdir, outdir, nz_lns=5, nck=2):
             to_parquet(cat_fn, index=False)
     return
 
+
+def pmap_load(map_fn, fgoodmap_fn, fgood_thold=0.0):
+    """
+    Loads a FLASK number counts map
+    """
+    nside_map = ft.read_header(map_fn, 1)['nside']
+    nside_msk = ft.read_header(fgoodmap_fn, 1)['nside']
+    assert nside_map >= nside_msk 
+
+    print("Reading", map_fn)
+    fgoodmap = hp.read_map(fgoodmap_fn, verbose=False)
+    ip_good, = np.where(fgoodmap > fgood_thold)
+    nc = hp.read_map(map_fn, verbose=False, dtype=int)
+    if nside_msk > nside_map:  # 'Down-grade' the ip_good accordingly
+        ang_mid = hp.pix2ang(nside_map, np.arange(hp.nside2npix(nside_map)))
+        mask_tmp = np.in1d(hp.ang2pix(nside_msk, *ang_mid), ip_good)
+        ip_good = np.arange(hp.nside2npix(nside_map))[mask_tmp]
+
+    return (nc[ip_good], ip_good, nside_map, fgoodmap[ip_good])
+
+
+def ncmap_sample_positions(nc_map, ip_good, nside, fgoodmap, nside_up=4):
+    """
+    Samples positions of galaxies inside a footprint from a number counts map
+    """
+    ip_good_nest = hp.ring2nest(nside, ip_good)
+    ipix_nest = [rd.sample(range(ip_good_nest[i] * 4**nside_up,
+                                 (ip_good_nest[i] + 1) * 4**nside_up),
+                           nc_map[i])
+                 for i in range(len(ip_good))]
+    ipix_nest = [ip for sub in ipix_nest for ip in sub]
+
+    hpix = hu.HealPix('nest', nside * 2**nside_up)
+    ra, dec = hpix.pix2eq(ipix_nest)
+    return ra, dec
+
+
+def lnscat_make_frompmap(map_fn, fgoodmap_fn, fgood_thold=0.0, seed=None):
+    """
+    Generates a lens position catalog for a FLASK simulation realization.
+    """
+    lnscat = pd.DataFrame(columns=['ra', 'dec'])
+
+    nc_map, ip_good, nside_map, fgoodmap = pmap_load(map_fn, fgoodmap_fn,
+                                                     fgood_thold=fgood_thold)
+
+    lnscat['ra'], lnscat['dec'] = ncmap_sample_positions(
+        nc_map, ip_good, nside_map, fgoodmap)
+
+    return lnscat
+
+
+def process_one_pmap(iseed, ick, iz, flaskdir, outdir):
+    """
+    Processes a single FLASK seed p-map
+    """
+    fgoodmap_fn = f"{flaskdir}/cookies/ck{ick+1}.fits.gz"
+    map_fn = f"{flaskdir}/4096/seed{iseed+1}/p-s{iseed+1}-f1z{iz+1}.fits"
+    cat_fn = f"{outdir}/lnscat_z{iz+1}_s{iseed+1}_ck{ick+1}.parquet"
+
+    lnscat = lnscat_make_frompmap(map_fn, fgoodmap_fn)
+    lnscat.to_parquet(cat_fn, index=False)
+
+    return cat_fn
+
+
 def load_inputcl(i, j, lmax,
                  idir="/global/cscratch1/sd/faoli/flask_desy3/Cl_flaskv2p0_nolimber_emu_Nsource4"):
     """
@@ -174,31 +236,44 @@ def load_inputcl(i, j, lmax,
 
 
 if __name__ == "__main__":
-    sys.stdout.flush()  # For the impatient people :).
-    
-    parser = ArgumentParser()
+    parser = ArgumentParser(description="FLASK post-processing")
+    parser.add_argument("conf", nargs=1,
+                        help="YAML configuration file")
     parser.add_argument("--iseed", type=int, required=True,
                         help="Seed index (0-based).")
-    parser.add_argument("--flaskdir",
-                        default="/global/cscratch1/sd/faoli/flask_desy3",
-                        help="FLASK root directory.")
-    parser.add_argument("--outdir",
-                        default="/global/cscratch1/sd/faoli/flask_desy3/maskedcats",
-                        help="Output directory.")
+    parser.add_argument("--des_release", required=True,
+                        help="y1 or y3 for the moment")
+    parser.add_argument("--processes", type=int,
+                        default=10,
+                        help="Number of processes for multiprocessing.")
     o = parser.parse_args()
 
-    # Y3, See cdcvs.fnal.gov/redmine/projects/des-theory/wiki/Y3_Analysis_Choices
-    # May need some cross-check from Troxel.
-    nbar = [1.476, 1.479, 1.484, 1.461]
-    sigma_e = [0.247, 0.266, 0.263, 0.314]
+    with open(o.conf[0], 'rt') as f:
+        conf = yaml.safe_load(f)
 
-    if not os.path.exists(o.outdir):
-        os.makedirs(o.outdir)
-    
-    args = [(o.iseed, ick, iz, o.flaskdir, o.outdir, nbar[iz], sigma_e[iz])
-            for iz, ick in it.product(range(len(nbar)), range(2))]
-    print(args)
-    with mp.Pool(processes=len(args)) as pool:
+    outdir = f"{conf['flaskdir']}/maskedcats"
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    # lnscat generation depends on if Y1 or Y3 release.
+    # Also the prefix names of FLASK products depend on that.
+    if o.des_release == 'y1':
+        kggpref = f"kgg-s{o.iseed+1}-f2"
+
+        args = [(o.iseed, ick, iz, conf['flaskdir'], outdir) for iz, ick
+                in it.product(range(conf['nz_lns']), range(conf['nck']))]
+        with mp.Pool(processes=o.processes) as pool:
+            cat_fn = pool.starmap(process_one_pmap, args)
+
+    elif o.des_release == 'y3':
+        process_lenscat(o.iseed, o.flaskdir, o.outdir)
+
+    else:
+        raise NotImplementedError("Currently only y1 and y3 des_release(s) "
+                                  + "are supported.")
+
+    args = [(o.iseed, ick, iz, conf['flaskdir'], outdir, conf['neff'][iz],
+             conf['sigma_e'][iz], kggpref) for iz, ick
+            in it.product(range(conf['nz_src']), range(conf['nck']))]
+    with mp.Pool(processes=o.processes) as pool:
         cat_fn = pool.starmap(process_one_kggmap, args)
-
-    process_lenscat(o.iseed, o.flaskdir, o.outdir)
