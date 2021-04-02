18b5e51fbcbf50daa1b865def7d9491b8efa6dbd
Hugo Camacho <hocamachoc@gmail.com>
2021-04-02 09:20:02 -0300

    Update y1flask generation

--- script/submit_flask.sh
+++ script/submit_flask.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Load conda env
-# module load miniconda/3
-module load python
+module load miniconda/3
+# module load python
+
 source activate 3x2pths
 
 uname -a
 
 time python3 flask.py etc/y1flask_csh.yml\
      --iseed ${SLURM_ARRAY_TASK_ID}\
+     --processes 2\
      --des_release y1
 
 # time python3 flask.py etc/y3flask_csh.yml\
