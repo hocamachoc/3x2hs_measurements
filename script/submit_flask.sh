09481036685f7e8601c3d3522d25d81c7756a4c6
Hugo Camacho <hocamachoc@gmail.com>
2021-04-03 08:33:43 -0300

    Update for DESY3 CSH

--- script/submit_flask.sh
+++ script/submit_flask.sh
@@ -8,13 +8,14 @@ source activate 3x2pths
 
 uname -a
 
-time python3 flask.py etc/y1flask_csh.yml\
+# time python3 flask.py etc/y1flask_csh.yml\
+#      --iseed ${SLURM_ARRAY_TASK_ID}\
+#      --processes 2\
+#      --des_release y1
+
+time python3 flask.py etc/y3flask_csh.yml\
      --iseed ${SLURM_ARRAY_TASK_ID}\
      --processes 2\
-     --des_release y1
-
-# time python3 flask.py etc/y3flask_csh.yml\
-#      --iseed ${SLURM_ARRAY_TASK_ID}\ 
-#      --des_release y3
+     --des_release y3
 
 conda deactivate
