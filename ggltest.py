ef43ecc9cd0a1da5899ecaf8a961a74062fda01c
Lucas James Faga <lucas.faga@usp.br>
2021-05-18 07:35:43 -0700

    add an option to measure DESY1 data (metacal + redmagic) (17/05/21)

--- ggltest.py
+++ ggltest.py
@@ -11,8 +11,6 @@ import flask
 import mcalcat
 import csh, gcl
 from functools import partial
-from os.path import exists, getsize
-
 print = partial(print, flush=True)  # For the impatient people :).
 
 # Configuration file
@@ -29,24 +27,6 @@ print(conf, odir)
 if not os.path.exists(odir):
     os.makedirs(odir)
 
-def mcm_process(ick, zj, dmask_i, cshmask_j, bins, mcm_dir):
-    """ Return MCM workspace for pos - pos
-    """
-    print(f'calculating mcm - ck{ick} ...', flush=True)
-    path = f"{mcm_dir}/mcm_ggl_ck{ick}_source_z{zj}.fits"
-    w = nmt.NmtWorkspace()
-    if exists(path) and getsize(path) > 0:
-        w.read_from(path)
-        print(f'mcm file already exists', flush=True)
-        return w
-    print(f"mcm file doesn't exist", flush=True)
-    f0 = nmt.NmtField(dmask_i, [dmask_i])
-    f2 = nmt.NmtField(cshmask_j, [cshmask_j, cshmask_j])
-    w.compute_coupling_matrix(f0, f2, bins)
-    w.write_to(path)
-    print(f'mcm file computed and saved at {path}', flush=True)
-    return w
-
 if conf['type'] == 'flask':
     real_id = int(sys.argv[2])   # Realization ID. Starts at 0
     iseed, ick = real_id // conf['nck'] + 1, real_id % conf['nck'] + 1
@@ -55,7 +35,7 @@ if conf['type'] == 'flask':
     # Prepare cosmic-shear stuff
     cshcat = [f"{conf['flaskdir']}/maskedcats" +
               f"/srccat_z{iz+1}_s{iseed}_ck{ick}.parquet"
-              for iz in range(conf['nz_src'])]
+              for iz in range(conf['nz_source'])]
     cshcat = [csh.cat_fromflsk(fn, conf['nside'], conf['nonoise'])
               for fn in cshcat]
 
@@ -64,9 +44,9 @@ if conf['type'] == 'flask':
     gclmask = gcl.mask_make(gclmask, ick, conf['nside'], odir)
     fsky = gclmask.mean()
     gclfield, nobj = [], []
-    print(f'creating clustering field',flush=True)
-    for iz in range(conf['nz_lns']): 
-        print(f'zbin {iz}',flush=True)
+    print(f'\n Calculating clustering fields ...', flush=True)
+    for iz in range(conf['nz_lens']): 
+        print(f'z{iz+1}', flush=True)
         gclcat = f"{conf['flaskdir']}/maskedcats"\
                  + f"/lnscat_z{iz+1}_s{iseed}_ck{ick}.parquet"
         gclcat = gcl.cat_fromflsk(gclcat, conf['nside'])
@@ -79,6 +59,25 @@ if conf['type'] == 'flask':
 
     # Output filename
     ofn = f'{odir}/cls_ggl_s{iseed}_ck{ick}.npz'
+
+elif conf['type'] == 'y1metacal':
+    cshcat = mcalcat.mcalcat_process(conf['mcalcat'], conf['zbin'],
+                                     conf['nside'],0)
+    ofn = f'{odir}/cls_ggl_mcal.npz'
+    
+    gclfield = []
+    for i in range(conf['nz_lns']):
+        wc = hp.read_map(f"{conf['redmagic']}/wcountsmap_zbin{i}.fits")
+        dmask = hp.read_map(f"{conf['redmagic']}/maskmap.fits")
+
+        nbar = sum(wc[dmask>0])/sum(dmask[dmask>0])
+        print(nbar)
+
+        dmap = np.full(len(dmask), 0.0)
+        dmap[dmask>0] = wc[dmask>0]/(nbar*dmask[dmask>0]) - 1
+
+        gclfield.append(nmt.NmtField(dmask, [dmap],  purify_e=False, purify_b=False))
+
 else:
     raise NotImplementedError(f"Computation type {conf['type']} not"
                               + " implemented")
@@ -92,31 +91,37 @@ if elledges[-1] < 3 * conf['nside']:
     elledges = np.append(elledges, 3 * conf['nside'])
 bins = nmt.NmtBin.from_edges(elledges[:-1], elledges[1:])
 
+w = nmt.NmtWorkspace()
+
 cls = {'ell_eff': bins.get_effective_ells()}
 
 if conf['nside'] <= 2048:
     field_i = gclfield
     field_j = []
-    print(f'creating shear field',flush=True)
+    print(f'\n Calculating shear fields ...', flush=True)
     for j in range(conf['nz_src']):
-        print(f'zbin {j}',flush=True)
+        print(f'z{j+1}', flush=True)
         cshcat_j = cshcat[j]
         cshmask_j = csh.mask_make(cshcat_j, conf['nside'])
         field_j.append(csh.field_make(cshcat_j, cshmask_j,
                                  save_maps=conf['save_maps'],
                                  maps_prefix=f'{odir}/zbin{j}'))
-
+    print(f'\n Computing MCM and getting cls ...', flush=True)
     for i in range(conf['nz_lns']):
         for j in range(conf['nz_src']):
-            print(f'computing cls z{i+1} z{j+1}', flush=True)
-            #w.compute_coupling_matrix(field_i[i], field_j[j], bins)
-            w = mcm_process(conf['nck'], j+1, gclmask, cshmask_j[j], bins, conf['mcm_dir'])
+            print(f'MCM z{i+1}z{j+1}', flush=True)
+            w.compute_coupling_matrix(field_i[i], field_j[j], bins)
+            if conf['save_mcm'] == True and conf['type'] == 'y1metacal':
+                path = f"{conf['mcm_dir']}/mcm_ggl_metacal_z{i}z{j}.fits"
+                w.write_to(path)
             cls[f'bpwrwin_{i}{j}'] = w.get_bandpower_windows()
+            print(f'getting coupled cls z{i+1}z{j+1}', flush=True)
             cls_coup = nmt.compute_coupled_cell(field_i[i], field_j[j])
             if conf['pixwin']:
                 cls_coup /= np.array([hp.pixwin(conf['nside'])] * 2)**2
+            print(f'decoupling cls z{i+1}z{j+1}', flush=True)
             cls[f'cl_{i}{j}'] = w.decouple_cell(cls_coup)
-            print('\n', flush=True)
+            print('\n')
 
 else: 
     for i in range(conf['nz_lns']):
