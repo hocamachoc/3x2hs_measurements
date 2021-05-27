e2f46a6d0f7bb9af00657a663c264acd4c06a4b1
Hugo Camacho <hocamachoc@gmail.com>
2021-05-27 05:01:28 -0300

    Add module purge to slurm submission script

--- script/submit_gcl.sh
+++ script/submit_gcl.sh
@@ -2,8 +2,9 @@
 #SBATCH --mail-type=ALL
 
 # Load conda env
-# module load miniconda/3
-module load python
+module purge
+module load miniconda/3
+# module load python
 source activate 3x2pths
 
 export OMP_PROC_BIND=true
@@ -11,6 +12,8 @@ export OMP_PLACES=threads
 export OMP_NUM_THREADS=$(lscpu| grep -e '^CPU(s):'| awk '{print $2}')
 echo "Max No. of threads: ${OMP_NUM_THREADS}"
 uname -a
+
+which python3
 
 # # FLASK Y1
 # time python3 gcltest.py etc/y1flask_gcl.yml ${SLURM_ARRAY_TASK_ID}
